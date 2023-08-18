39ce6988ccc877c9032cb59e33feb1af4f140010
fideleflutter <fideledagne2@gmail.com>
2023-08-18 09:18:41 -0700

    alx

--- 0x04-more_functions_nested_loops/10-main.c
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -1,30 +1,15 @@
 #include "main.h"
+
 /**
-*print_triangle - prints a triangle
-*@size:size parameter of triangle
-*Return:returns nothing
-*author fideleflutter
-*/
-void print_triangle(int size)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-int inc1, inc2;
-
-if (size > 0)
-{
-for (inc1 = 1; inc1 <= size; inc1++)
-{
-for ((inc2 = size - inc1); inc2 > 0; inc2--)
-_putchar(' ');
-
-for (inc2 = 0; inc2 < inc1; inc2++)
-_putchar('#');
-
-if (inc1 == size)
-continue;
-
-_putchar('\n');
+    print_triangle(2);
+    print_triangle(10);
+    print_triangle(1);
+    print_triangle(0);
+    return (0);
 }
-}
-_putchar('\n');
-}
-
