774b2ad8c2de7000d5b64ddd0739e6d590ea5eca
fideleflutter <fideledagne2@gmail.com>
2023-08-18 08:49:14 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*mul - function that multipliestwo numbers
+*@a: first number
+*@b: second number
+*Return: returns result
+*author fidele
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
