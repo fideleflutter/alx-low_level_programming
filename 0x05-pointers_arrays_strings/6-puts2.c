5a79f7b3665701cbda9b4af2f44d3bb09d7ab377
fideleflutter <fideledagne2@gmail.com>
2023-08-21 08:27:04 -0700

    alx

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,26 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
- * puts2 - prints part of a string
- * @str: string to be printed
- *
- * Return: void
+ * puts2 - prints every other character of a string followed by a new line
+ * @str: Pointer to the string
+ * Return: Always 0 (Success)
+ * Author: fideleflutter
  */
+
 void puts2(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-if (i % 2 == 0)
-_putchar(str[i]);
-i++;
-}
+	int length = strlen(str);
+	char *end = str + length;
 
-_putchar('\n');
+	while (str < end)
+	{
+		printf("%c", *str);
+		str = str + 2;
+	}
+
+	printf("\n");
 }
 
 
